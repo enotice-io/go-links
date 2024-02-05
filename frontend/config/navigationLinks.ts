0bd4979d04445adab3c5e364c4c246c03f01163c
yaz-saleh <74604867+yaz-saleh@users.noreply.github.com>
2024-02-05 12:46:12 -0500

    fix: extension url (#12)

--- frontend/config/navigationLinks.ts
+++ frontend/config/navigationLinks.ts
@@ -11,5 +11,5 @@ export const navigationLinks = {
   LOGIN_MICROSOFT: 'https://id.trot.to/sso/o365?redirect_to=https%3A%2F%2Ftrot.to',
   LOGOUT: '/_/auth/logout',
   EXTENSION:
-    'https://chrome.google.com/webstore/detail/trotto-go-links/nkeoojidblilnkcbbmfhaeebndapehjk',
+    'https://chromewebstore.google.com/detail/bolinks/imhbpbhfimipoogficnejkpclibacoil',
 }
